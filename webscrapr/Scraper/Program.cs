3c6f484a96a75e70cb83d1c8bc16a8381b463fd5
ombakhshi <ombakh28@gmail.com>
2024-12-16 12:14:02 -0500

    added ability to paste amazon link and price will be returned

--- webscrapr/Scraper/Program.cs
+++ webscrapr/Scraper/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     public static async Task Main(string[] args)
     {
-        var url = "https://www.amazon.com/Mens-Y-Eau-Parfum-3-3-oz/dp/B07GB6B37N/?_encoding=UTF8&pd_rd_w=fPwu8&content-id=amzn1.sym.255b3518-6e7f-495c-8611-30a58648072e%3Aamzn1.symc.a68f4ca3-28dc-4388-a2cf-24672c480d8f&pf_rd_p=255b3518-6e7f-495c-8611-30a58648072e&pf_rd_r=QYNQ5S4HCBN6TN36MM5N&pd_rd_wg=PGuaS&pd_rd_r=d19fc31a-5d53-48fc-af71-f341fd796743&ref_=pd_hp_d_atf_ci_mcx_mr_ca_hp_atf_d";
-
+        Console.Write("Paste your Amazon link: ");
+        var url = Console.ReadLine();
         // Start Playwright
         using var playwright = await Playwright.CreateAsync();
         var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
